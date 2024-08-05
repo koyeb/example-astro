80de9b3be2fc22c1f35cdb2e29c309be45634f67
Muhammad Ofie <ofieisranta@gmail.com>
2024-08-05 07:58:18 +0700

    feat: landing page unfinished

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,18 @@
 // tailwind.config.cjs
 const { nextui } = require("@nextui-org/react");
+import defaultTheme from 'tailwindcss/defaultTheme'
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     // ...
-    "./src/**/*.{astro,js,jsx,ts,tsx}",
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Poppins', '"Montserrat Variable"', ...defaultTheme.fontFamily.sans],
+      },
       screens: {
         sm: "640px",
         md: "768px",
