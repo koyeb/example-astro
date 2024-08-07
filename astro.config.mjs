1bbf570fa318a0979c3bb744ed3fb0ad5914b67b
Muhammad Ofie <ofieisranta@gmail.com>
2024-08-07 20:32:35 +0700

    fix: build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,22 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    port: 4321,
+    host: true
+  },
   integrations: [tailwind(), react()],
   vite: {
     ssr: {
-      noExternal: ['@fontsource-variable/montserrat'],
+      noExternal: ['@fontsource-variable/montserrat']
     }
   },
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
