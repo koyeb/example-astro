379bc769ad7093040788b3a75cf01192d9a665a4
Fawad <fawad.naeem.qureshi@gmail.com>
2024-12-01 22:24:18 +0500

    Fixed multiple config issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
@@ -6,6 +7,14 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  server: {
+    port: parseInt(process.env.PORT) || 4321,
+    host: true
+  },
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  }),
+  site: "https://slashexperts.com",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
