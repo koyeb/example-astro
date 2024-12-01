4632a1a415f6e200ca5ae8972aef462c683ef23f
Fawad <fawad.naeem.qureshi@gmail.com>
2024-12-01 22:00:11 +0500

    SLashExperts Example company website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
-import { defineConfig } from 'astro/config';
-
-import node from "@astrojs/node";
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    port: parseInt(process.env.PORT) || 4321,
-    host: true
-  },
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  site: "https://astroship.web3templates.com",
+  integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
